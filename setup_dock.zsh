99d86aad7d79ad907c33476233c7f33247b0af9c
Mathieu Desvé <mathieu.desve@gmail.com>
2021-11-07 20:19:36 +0100

    Setup dock icon order

---
+++ setup_dock.zsh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+
+set -e
+
+__dock_item() {
+    printf '%s%s%s%s%s' \
+           '<dict><key>tile-data</key><dict><key>file-data</key><dict>' \
+           '<key>_CFURLString</key><string>' \
+           "$1" \
+           '</string><key>_CFURLStringType</key><integer>0</integer>' \
+           '</dict></dict></dict>'
+}
+
+printf '%s' 'Setting up Dock icons...'
+defaults write com.apple.dock \
+               persistent-apps -array "$(__dock_item /System/Applications/System\ Preferences.app)" \
+                                      "$(__dock_item /Applications/VeraCrypt.app)" \
+                                      "$(__dock_item /Applications/1Password\ 7.app)" \
+                                      "$(__dock_item /Applications/iTerm.app)" \
+                                      "$(__dock_item /Applications/Visual\ Studio\ Code.app)" \
+                                      "$(__dock_item /Applications/Spotify.app)" \
+                                      "$(__dock_item /Applications/Google\ Chrome.app)"
+killall Dock
+printf '%s\n' ' done.'
