625b3fd40a0b29aa47552d25da6840507388d737
Mathieu Desvé <mathieu.desve@gmail.com>
2021-12-10 15:18:28 +0100

    Refactor node script install

--- setup_node.zsh
+++ setup_node.zsh
@@ -10,19 +10,40 @@ echo "\n<<< Starting Node Setup >>>\n"
 source $(brew --prefix nvm)/nvm.sh
 if [ ! -d "${HOME}/.nvm" ]; then
   mkdir ~/.nvm
-  nvm install node
-  nvm install --lts
-  nvm install 14
-
-  nvm alias default lts/*
 fi
 
-nvm use --lts
+while read v; do
+  echo "Node: $v"
+  CURRENT=`nvm version $v`
+  NEW=`nvm version-remote $v`
 
-# Install Global NPM Packages
-npm install --global typescript
-npm install --global http-server
-npm install --global aws-cdk
+  # Install or Update if needed
+  if [ "$CURRENT" = "N/A" ]; then
+    echo " -> Install version $NEW"
+    nvm install $v
+  elif ! [ "$CURRENT" = "$NEW" ]; then
+    echo " -> Update version $CURRENT to $NEW"
+    nvm install $NEW --reinstall-packages-from=$CURRENT
+    nvm uninstall $CURENNT
+  else
+    echo " -> Version $CURRENT is up to date"
+  fi
 
-echo "Global NPM Packages Installed:"
-npm list --global --depth=0
+  nvm use $v
+
+  # Global packages
+  echo " -> Manage global modules"
+  while read p; do
+    if ! npm list -g $p > /dev/null 2>/dev/null; then
+      echo "   -> install $p"
+      npm install -g $p
+    fi
+  done < node/modules/$v
+  npm update -g
+  npm list --global --depth=0
+
+done < node/versions
+
+DEFAULT=`cat node/default`
+nvm alias default $DEFAULT
+nvm use $DEFAULT
