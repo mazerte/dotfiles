b30ade4ea583882390650c7237f22f27b187dc18
Mathieu Desvé <mathieu.desve@gmail.com>
2022-01-29 14:13:30 +0100

    Add user-data example for ec2 instances

---
+++ .devcontainer/user-data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y docker
+
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
+
+echo ' {"hosts": ["tcp://0.0.0.0:2375", "unix:///var/run/docker.sock"]}' | sudo tee /etc/docker/daemon.json
+sudo mkdir /etc/systemd/system/docker.service.d
+cat << EOF | sudo tee /etc/systemd/system/docker.service.d/override.conf
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
