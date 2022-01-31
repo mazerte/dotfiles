8d0e67c5c79bd380d0481e97ddfcc7028eeda4cb
Mathieu Desvé <mathieu.desve@gmail.com>
2022-01-31 23:06:15 +0100

    Fix amzn2 install

--- amzn/amazon-linux-2-user-data.sh
+++ amzn/amazon-linux-2-user-data.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 sudo yum install -y git zsh
+
+sudo su ssm-user
+git clone https://github.com/mazerte/dotfiles ~/.dotfiles
+cd ~/.dotfiles
+./install
+
+sudo su ec2-user
 git clone https://github.com/mazerte/dotfiles ~/.dotfiles
 cd ~/.dotfiles
 ./install
