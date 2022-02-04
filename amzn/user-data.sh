c8ceb9ca5cede8f9089b7e94daf4a8e54d111558
Mathieu Desvé <mathieu.desve@gmail.com>
2022-02-04 16:37:31 +0100

    Support ubuntu and compile user-data

---
+++ amzn/user-data.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  CURRENT_OSTYPE="linux"
+  CURRENT_ARCH=$(/usr/bin/arch)
+  CURRENT_LINUX_OS=$(egrep '^ID=' /etc/os-release | cut -d "=" -f 2 | sed 's/\"//g')
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  CURRENT_OSTYPE="macos"
+  CURRENT_ARCH=$(/usr/bin/arch)
+else
+  CURRENT_OSTYPE="unknown"
+fi
+
+# Install SSM
+if [[ $(sudo ps -ax | grep amazon-ssm-agents | wc -l) -eq 1 ]]; then
+  EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+  EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
+  ARCH=$([ "$(uname -m)" = "arm64" ] && echo "arm64" || echo "amd64")
+  mkdir /tmp/ssm
+  cd /tmp/ssm
+  if [[ "$CURRENT_LINUX_OS" == "debian" ]]; then
+    wget https://s3.$EC2_REGION.amazonaws.com/amazon-ssm-$EC2_REGION/latest/debian_$ARCH/amazon-ssm-agent.deb
+    sudo dpkg -i amazon-ssm-agent.deb
+    sudo systemctl status amazon-ssm-agent
+    sudo systemctl enable amazon-ssm-agent
+  elif [[ "$CURRENT_LINUX_OS" == "ubuntu" ]]; then
+    sudo snap install amazon-ssm-agent --classic
+  elif [[ "$CURRENT_OSTYPE" == "macos" ]]; then
+    sudo wget https://s3.$EC2_REGION.amazonaws.com/amazon-ssm-$EC2_REGION/latest/darwin_$ARCH/amazon-ssm-agent.pkg
+    sudo installer -pkg amazon-ssm-agent.pkg -target /
+    sudo launchctl load -w /Library/LaunchDaemons/com.amazon.aws.ssm.plist && sudo launchctl start com.amazon.aws.ssm
+  if
+fi
+
+if [[ "$CURRENT_OSTYPE" == "linux" ]]; then
+  if [[ "$CURRENT_LINUX_OS" == "debian" ]] || [[ "$CURRENT_LINUX_OS" == "ubuntu" ]]; then
+    sudo apt update -y
+    sudo apt install -y git zsh
+    if [[ "$CURRENT_LINUX_OS" == "ubuntu" ]]; then
+      sudo adduser --system  --disabled-password --shell /bin/zsh mazerte
+    else
+      sudo adduser --system --shell /bin/zsh mazerte
+    fi
+  elif [[ "$CURRENT_LINUX_OS" == "amzn" ]]; then
+    sudo yum install -y git zsh
+    sudo adduser -r -m -s /bin/zsh mazerte
+  fi
+  echo 'mazerte ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
+fi
+
+su mazerte <<'EOF'
+git clone https://github.com/mazerte/dotfiles ~/.dotfiles
+cd ~/.dotfiles
+./install
+EOF
--- setup_ubuntu_debian.sh
+++ setup_ubuntu_debian.sh
@@ -9,16 +9,35 @@ install-linux-packages() {
   echo "* Install following packages:"
   echo "-----------------------------------------------------------"
 
-  sudo apt-get update
+  sudo apt-get update -y
   sudo apt-get install -y apt-file aptitude deborphan
   sudo apt-get install -y build-essential libreadline-dev
   sudo apt-get install -y zsh curl wget git tree unzip ncdu tmux trash-cli
   sudo apt-get install -y lsof whois traceroute lshw
   sudo apt-get install -y net-tools iputils-tracepath dnsutils
   sudo apt-get install -y netcat-openbsd
-  sudo apt-get install -y exa jq vim gnupg csvkit
+  sudo apt-get install -y jq vim gnupg csvkit
   sudo apt-get install -y zsh-theme-powerlevel9k
-  sudo apt-get install -y bat kubecolor
+  sudo apt-get install -y bat
+
+  if [[ "$CURRENT_LINUX_OS" == "ubuntu" ]]; then
+    ARCH=$(/usr/bin/arch)
+    sudo apt-get install -y awscli
+
+    # Kubecolor
+    KUBECOLOR_VERISON="0.0.20"
+    wget -O /tmp/kubecolor.tar.gz https://github.com/hidetatz/kubecolor/releases/download/v${KUBECOLOR_VERISON}/kubecolor_${KUBECOLOR_VERISON}_Linux_$ARCH.tar.gz
+    tar -xvzf /tmp/kubecolor.tar.gz -C /tmp
+    sudo mv /tmp/kubecolor /usr/local/bin/kubecolor
+
+    # Exa
+    EXA_VERISON="0.8.0"
+    wget -O /tmp/exa.tar.gz https://github.com/ogham/exa/releases/download/v${EXA_VERISON}/exa-linux-$ARCH-${EXA_VERISON}.zip
+    unzip /tmp/exa.tar.gz -d /tmp
+    sudo mv /tmp/exa-linux-$ARCH /usr/local/bin/exa
+  elif
+    sudo apt-get install -y exa kubecolor
+  fi
 }
 
 install-antibody(){
