c197c6483565b599398280f8ad4f33553e90e96d
Mathieu Desvé <mathieu.desve@gmail.com>
2022-02-05 23:44:18 +0100

    Fix rhel on arm64

--- amzn/user-data.sh
+++ amzn/user-data.sh
@@ -23,13 +23,11 @@ if [[ $(sudo ps -ax | grep amazon-ssm-agents | wc -l) -eq 1 ]]; then
   if [[ "$CURRENT_LINUX_OS" == "debian" ]]; then
     wget https://s3.$EC2_REGION.amazonaws.com/amazon-ssm-$EC2_REGION/latest/debian_$ARCH/amazon-ssm-agent.deb
     sudo dpkg -i amazon-ssm-agent.deb
-    sudo systemctl status amazon-ssm-agent
     sudo systemctl enable amazon-ssm-agent
   elif [[ "$CURRENT_LINUX_OS" == "ubuntu" ]]; then
     sudo snap install amazon-ssm-agent --classic
   elif [[ "$CURRENT_LINUX_OS" == "rhel" ]]; then
     sudo dnf install -y https://s3.$EC2_REGION.amazonaws.com/amazon-ssm-$EC2_REGION/latest/linux_$ARCH/amazon-ssm-agent.rpm
-    sudo systemctl status amazon-ssm-agent
     sudo systemctl enable amazon-ssm-agent
   elif [[ "$CURRENT_OSTYPE" == "macos" ]]; then
     sudo wget https://s3.$EC2_REGION.amazonaws.com/amazon-ssm-$EC2_REGION/latest/darwin_$ARCH/amazon-ssm-agent.pkg
