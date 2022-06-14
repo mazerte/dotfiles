9aacede3055effa7b34487230013954a447211f4
Mathieu Desvé <mathieu.desve@gmail.com>
2022-06-14 10:09:10 +0200

    Fix ssh setup in codespace

--- setup_ssh.zsh
+++ setup_ssh.zsh
@@ -1,9 +1,15 @@
 #!/usr/bin/env zsh
 
+if [ -e /workspaces/.codespaces/.persistedshare/dotfiles ]; then
+  DOTFILES="/workspaces/.codespaces/.persistedshare/dotfiles"
+else
+  DOTFILES="$HOME/.dotfiles"
+fi
+
 if [ -e $HOME/.ssh/authorized_keys ]; then
   echo ".ssh and authorized keys exist"
 else
   touch $HOME/.ssh/authorized_keys
   chmod 0600 $HOME/.ssh/authorized_keys
-  cat ~/.dotfiles/ssh/authorized_keys >> $HOME/.ssh/authorized_keys
+  cat $DOTFILES/ssh/authorized_keys >> $HOME/.ssh/authorized_keys
 fi
